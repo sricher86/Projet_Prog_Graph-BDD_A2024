886cd32c55354407c87a5509634bd650cb9378e2
Akihsra18 <154715861+Akihsra18@users.noreply.github.com>
2024-11-27 10:29:56 -0500

    Creating pages etc.

--- Projet_Prog_Graph-BDD_A2024/Projet_Prog_Graph-BDD_A2024/PageAdminCompte.xaml.cs
+++ Projet_Prog_Graph-BDD_A2024/Projet_Prog_Graph-BDD_A2024/PageAdminCompte.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,6 +5,11 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -19,18 +19,13 @@ using Windows.Foundation.Collections;
 namespace Projet_Prog_Graph_BDD_A2024
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class PageAdminCompte : Page
     {
-        public MainWindow()
+        public PageAdminCompte()
         {
             this.InitializeComponent();
-
-            //mainFrame.Navigate(typeof(PageAuthentification));
-
-            mainFrame.Navigate(typeof(PageAdministrateur));
-
         }
     }
 }
